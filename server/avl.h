3c4d82e8a0e329f466285a3fa4821abd0b98351a
Nektarios <nektarioszampetoulakis@outlook.com>
2025-01-09 19:40:28 -0500

    Added event loops and timers ( part 12 )

--- server/avl.h
+++ server/avl.h
@@ -5,12 +5,25 @@
 #ifndef AVL_H
 #define AVL_H
 
+#include <stddef.h>
+#include <stdint.h>
 
 
-class avl {
-
+struct AVLNode {
+    uint32_t depth = 0;
+    uint32_t cnt = 0;
+    AVLNode *left = NULL;
+    AVLNode *right = NULL;
+    AVLNode *parent = NULL;
 };
 
+inline void avl_init(AVLNode *node) {
+    node->depth = 1;
+    node->cnt = 1;
+    node->left = node->right = node->parent = NULL;
+}
 
-
+AVLNode *avl_fix(AVLNode *node);
+AVLNode *avl_del(AVLNode *node);
+AVLNode *avl_offset(AVLNode *node, int64_t offset);
 #endif //AVL_H
